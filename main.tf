7ae4d07fe3aae62f7f92bc5bcb0e8173515c24f6
Sebastian Mangelkramer <sebastian.mangelkramer@fullstacks.eu>
2021-09-28 15:44:00 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,6 +16,7 @@ resource "opennebula_virtual_machine" "snack_session" {
   context = {
     HOSTNAME       = "$NAME"
     USERNAME       = "ubuntu"
+    START_SCRIPT_BASE64 = "YXB0IGluc3RhbGwgbmdpbngKZWNobyAiSGVsbG8gcGVnYXN1cyBOZXh0R2VuIENsb3VkIiA+IC92YXIvd3d3L2h0bWwvaW5kZXguaHRtbCA="
     SSH_PUBLIC_KEY = tls_private_key.key.public_key_openssh
   }
 
@@ -35,18 +36,18 @@ resource "opennebula_virtual_machine" "snack_session" {
     network_id = data.opennebula_virtual_network.vmnetwork.id
   }
 
-  provisioner "remote-exec" {
-    inline = [
-      "uname -a",
-    ]
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "uname -a",
+  #   ]
 
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = tls_private_key.key.private_key_pem
-      host        = self.nic.0.computed_ip
-    }
-  }
+  #   connection {
+  #     type        = "ssh"
+  #     user        = "ubuntu"
+  #     private_key = tls_private_key.key.private_key_pem
+  #     host        = self.nic.0.computed_ip
+  #   }
+  # }
 
 }
 
