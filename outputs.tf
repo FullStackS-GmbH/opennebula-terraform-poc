5d35a9e03da6a25d2922feefb71e49a4443b734f
Sebastian Mangelkramer <sebastian.mangelkramer@fullstacks.eu>
2021-09-28 15:33:42 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "vms" {
     for index, vm in opennebula_virtual_machine.snack_session :
     {
       name = vm.name
-      ip   = opennebula_virtual_machine.snack_session[index].computed_ip
+      ip   = vm.nic.0.computed_ip
     }
   ]
 }
