42cd806821b034549a8260881ad9f853c610e1b0
Sebastian Mangelkramer <sebastian.mangelkramer@fullstacks.eu>
2021-09-29 20:37:05 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,13 +10,13 @@ resource "opennebula_virtual_machine" "snack_session" {
   vcpu        = var.vm_cpu
   memory      = var.vm_ram
   template_id = data.opennebula_template.vmtemplate.id
-  group       = "oneadmin"
+  group       = "snacksession"
   permissions = "660"
 
   context = {
     HOSTNAME       = "$NAME"
     USERNAME       = "ubuntu"
-    START_SCRIPT_BASE64 = "YXB0IGluc3RhbGwgbmdpbngKZWNobyAiSGVsbG8gcGVnYXN1cyBOZXh0R2VuIENsb3VkIiA+IC92YXIvd3d3L2h0bWwvaW5kZXguaHRtbCA="
+    #START_SCRIPT_BASE64 = "YXB0IGluc3RhbGwgbmdpbngKZWNobyAiSGVsbG8gcGVnYXN1cyBOZXh0R2VuIENsb3VkIiA+IC92YXIvd3d3L2h0bWwvaW5kZXguaHRtbCA="
     SSH_PUBLIC_KEY = tls_private_key.key.public_key_openssh
   }
 
@@ -36,18 +36,19 @@ resource "opennebula_virtual_machine" "snack_session" {
     network_id = data.opennebula_virtual_network.vmnetwork.id
   }
 
-  # provisioner "remote-exec" {
-  #   inline = [
-  #     "uname -a",
-  #   ]
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt -y install nginx",
+    ]
 
-  #   connection {
-  #     type        = "ssh"
-  #     user        = "ubuntu"
-  #     private_key = tls_private_key.key.private_key_pem
-  #     host        = self.nic.0.computed_ip
-  #   }
-  # }
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = tls_private_key.key.private_key_pem
+      host        = self.nic.0.computed_ip
+    }
+  }
 
 }
 
